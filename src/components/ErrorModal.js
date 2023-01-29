4e3ff2da18b679e212f8bf774a2659032e7eecb0
Mario <cerdamario13@gmail.com>
2023-01-28 20:24:55 -0600

    adding a portal

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,24 +1,52 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import classes from './ErrorModal.module.css';
+import Button from "react-bootstrap/Button";
+import classes from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
+
+/*
+  In React, a portal allows you to render a component or elements at a different 
+  place in the DOM tree than the component's parent. 
+  This can be useful for things like modals, tooltips, and other elements 
+  that need to be rendered outside of the normal flow of the page.
+*/
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+};
+
+const Modal = (props) => {
+  return (
+    <div className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </div>
+  );
+};
 
 const ErrorModal = (props) => {
-    return(
-      <div>
-        <div className={classes.backdrop} onClick={props.onConfirm}></div>
-        <div className={classes.modal}>
-          <header className={classes.header}>
-            <h2>{props.title}</h2>
-          </header>
-          <div className={classes.content}>
-            <p>{props.message}</p>
-          </div>
-          <footer className={classes.actions}>
-            <Button onClick={props.onConfirm}>Okay</Button>
-          </footer>
-        </div>
-      </div>
-    )
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("modal-root")
+      )}
+    </>
+  );
 };
 
 export default ErrorModal;
