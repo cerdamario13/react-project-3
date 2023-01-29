1d8c033f32734352f646bf632955a9aa08bc54a4
Mario <cerdamario13@gmail.com>
2023-01-28 19:42:47 -0600

    delete the users if they are clicked

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import UserForm from './components/Form';
 import UserList from './components/UserList';
 
+
 function App() {
   
   //For user list
@@ -10,14 +11,14 @@ function App() {
   
   const addUser = (uName, uAge) => {
     setUsers((prevUsersList) => {
-      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}];
+      return [...prevUsersList, {key: Math.random().toString(), name: uName, age: uAge, id: Math.random().toString()}];
     });
   };
   
   return (
     <>
       <UserForm onFormSubmit={addUser}></UserForm>
-      <UserList users={users}></UserList>
+      <UserList users={users} setUsers={setUsers}></UserList>
     </>
   );
 }
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,14 +2,32 @@ import React from "react";
 import './UserList.css';
 
 const UserList = (props) => {
+  
+  //delete the users if the user is clicked
+  const deleteUser = (modeId) => {
+    
+    //make a copy of the array
+    var array = props.users.slice();
+    
+    // find the object by using id and also the index
+    var selected = array.filter(id => id['id'] === modeId);
+    var index = array.indexOf(selected[0]);
+    
+    //remove the selected object and update the list
+    array.splice(index, 1);
+    props.setUsers(array);
+    
+  }
+  
+  
   return (
     <>
       <ul className="users">
         { props.users.length ?
         (props.users.map((user) => (
-          <li key={user.id}>
-            {user.name} ({user.age}) Years Old
-          </li>
+          <div className="users ul" onClick={() => deleteUser(user.id)} style={{cursor:'pointer'}}>
+            {user.name} ({user.age} Years Old)
+          </div>
         ))) : (<div>No Users</div>)
       }
       </ul>
