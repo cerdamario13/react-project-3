cac78cebb67f2ab70c195e2511578f5b026b5dd5
Mario <cerdamario13@gmail.com>
2023-02-12 12:35:27 -0600

    useEffect to check login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import MainPage from './components/MainPage';
 import Login from './components/Login';
@@ -8,15 +8,26 @@ function App() {
   
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   
+    // Check when the app restarts or the user refreshes if the user is logged in
+    useEffect(() => {
+      const storedUserInfo = localStorage.getItem('isLoggedIn');
+      
+      if (storedUserInfo === '1') {
+        setIsLoggedIn(true);
+      }
+      //This will only run once the app is started
+    }, []);
+  
   const logHandler = (email, password) => {
     //Check the email and password
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true); //temp login
   };
+    
   
   const logoutHandler = () => {
     setIsLoggedIn(false);
   };
-  
   
   return (
     <>
